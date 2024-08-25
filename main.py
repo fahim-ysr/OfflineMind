d59ce9c0285584b0e2bb47ed3a1a5c1b7dc9e3c5
fahim-ysr <fahim.yasir2001@gmail.com>
2024-08-25 18:34:38 -0230

    Created a simple AI chatbot using Llama3 model and a sample question

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # Initialized python file and installed all necessary langchain libraries
+
+# Make sure you have downloaded Ollama in your system
+
+#Then create a venv and install all the required modules using `pip install langchain langchain-ollama ollama`
+
+from langchain_ollama import OllamaLLM
+
+# Can replace the model name with other models
+model = OllamaLLM(model="llama3")
+
+res = model.invoke(input= "Hello, what are you?")
+print(res)
