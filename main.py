ea28498b32affa8e9469007ef552f3caed94541b
fahim-ysr <fahim.yasir2001@gmail.com>
2024-08-27 21:00:11 -0230

    Created a full version of AI Chatbot using Ollama and Python

--- main.py
+++ main.py
@@ -5,9 +5,27 @@
 #Then create a venv and install all the required modules using `pip install langchain langchain-ollama ollama`
 
 from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+# Passing multi-line string as a template for describing the instructions
+template = """
+Answer the question below.
+
+Here is the conversation history: {context}
+
+Question: {question}
+
+Answer: 
+"""
 
 # Can replace the model name with other models
 model = OllamaLLM(model="llama3")
 
-res = model.invoke(input= "Hello, what are you?")
+prompt = ChatPromptTemplate.from_template(template)
+
+# Creating a chain of both prompt and model
+chain = prompt | model
+
+res = chain.invoke({"context": "", "question": "hey how are you?"})
 print(res)
+
